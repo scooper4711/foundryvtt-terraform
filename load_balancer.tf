95304235ff6733c302178ece9ec129397c1a5712
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-14 20:51:58 -0700

    fixing syntax

--- load_balancer.tf
+++ load_balancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "foundry-loadbalancer" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_foundry.id]
-  subnets            = aws_default_subnet.default_az1.id
+  subnets            = [aws_default_subnet.default_az1.id]
 
   enable_deletion_protection = true
 
