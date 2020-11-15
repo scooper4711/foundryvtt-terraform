e022f6cb278ef01af718d3dde9ce60bc0bacdf13
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-14 21:01:02 -0700

    two subnets

--- subnet.tf
+++ subnet.tf
@@ -5,3 +5,10 @@ resource "aws_default_subnet" "default_az1" {
     Name = "Default subnet for us-west-2a"
   }
 }
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Default subnet for us-west-2b"
+  }
+}
