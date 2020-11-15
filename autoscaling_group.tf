ad3868556761e905a7f7c3f9c2cbdf6be3a30a3d
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-14 22:15:37 -0700

    attach asg to lb

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -5,9 +5,14 @@ resource "aws_autoscaling_group" "foundryvtt_autoscaling_group" {
   desired_capacity   = 1
   max_size           = 1
   min_size           = 1
-
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
   launch_template {
     id      = aws_launch_template.foundryvtt_launchtemplate.id
     version = "$Latest"
   }
+}
+
+resource "aws_autoscaling_attachment" "foundryvtt_asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.foundryvtt_autoscaling_group.id
+  elb                    = aws_lb.foundry_loadbalancer.id
 }
