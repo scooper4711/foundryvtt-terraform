ef02d5511bdfdfda1a9942e543303ae9269f93c8
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-14 22:18:57 -0700

    naming convention

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,11 +1,11 @@
 resource "aws_lb_target_group" "foundry_lb_tg" {
-  name     = "foundry_lb_tg"
+  name     = "foundry-lb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_default_vpc.default.id
 }
 resource "aws_lb" "foundry_loadbalancer" {
-  name               = "foundry_loadbalancer"
+  name               = "foundry-loadbalancer"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_http.id]
