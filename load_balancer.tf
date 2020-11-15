049d0f053819bd056ff811c411d95604de4da947
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-14 21:44:14 -0700

    added target group

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,8 +1,14 @@
+resource "aws_lb_target_group" "test" {
+  name     = "tf-foundry-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
 resource "aws_lb" "foundry-loadbalancer" {
   name               = "foundry-loadbalancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_foundry.id]
+  security_groups    = [aws_security_group.allow_http.id]
   subnets            = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
 
   enable_deletion_protection = true
--- security_group.tf
+++ security_group.tf
@@ -1,3 +1,27 @@
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "Foundry from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
 resource "aws_security_group" "allow_foundry" {
   name        = "allow_foundry"
   description = "Allow Foundry inbound traffic"
