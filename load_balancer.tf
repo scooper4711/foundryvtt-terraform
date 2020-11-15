90d1e6484f5489a2ab0748a349dc6e425f51497a
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-14 21:01:44 -0700

    two subnets

--- load_balancer.tf
+++ load_balancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "foundry-loadbalancer" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_foundry.id]
-  subnets            = [aws_default_subnet.*.id]
+  subnets            = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
 
   enable_deletion_protection = true
 
