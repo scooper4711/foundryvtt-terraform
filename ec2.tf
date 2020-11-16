d3d905e46fb548d51b8c7fb608014490a69c2db7
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-15 20:16:13 -0700

    wip

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,16 @@
 
 data "aws_ami" "foundry-ami" {
-  executable_users = ["self"]
   most_recent      = true
-  name_regex       = "^foundryvtt0.7.6"
   owners           = ["063843753876"]
+  filter {
+    name   = "name"
+    values = ["foundryvtt*"]
+  }
 }
 
 resource "aws_instance" "foundry" {
   # us-west-2
-  ami           = "foundry-ami-"
+  ami           = aws_ami.foundry-ami.id
   instance_type = "t2.micro"
   user_data     = filebase64("${path.module}/startup.sh")
   subnet_id  = aws_default_subnet.default_az1.id
