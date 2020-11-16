14c832be008e772a90f837c9a38d7bc04cc39a76
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-15 21:05:29 -0700

    Made the instance size a variable

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "home_cidr" {
-    type = "string"
+    type        = string
     description = "The CIDR address (/32) to allow SSH for"
 }
 
 variable "ami_owner" {
-    type = "string"
+    type        = string
     description = "The owner id of the AMI for Foundry"
+}
+
+variable "instance_size" {
+    type        = string
+    description = "The size of the instance to run"
 }
