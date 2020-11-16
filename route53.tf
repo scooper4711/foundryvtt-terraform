d457b306d0c57d177e839f64709df721724ff62e
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-15 17:20:00 -0700

    remove invalid attribute

--- route53.tf
+++ route53.tf
@@ -5,7 +5,6 @@ resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www.inharnsway.com"
   type    = "A"
-  ttl     = "300"
   alias {
     name                   = aws_lb.foundry_loadbalancer.dns_name
     zone_id                = aws_lb.foundry_loadbalancer.zone_id
