6484fd341e06529f901127ea379f0e1aea2a4f42
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-15 21:09:31 -0700

    actually made it a variable

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ data "aws_ami" "foundry_ami" {
 
 resource "aws_instance" "foundry" {
   ami             = data.aws_ami.foundry_ami.id
-  instance_type   = "t4g.micro"
+  instance_type   = var.instance_size
   user_data       = filebase64("${path.module}/startup.sh")
   subnet_id       = aws_default_subnet.default_az1.id
   security_groups = [aws_security_group.allow_http.id,aws_security_group.ssh_from_home.id]
