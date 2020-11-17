cc6f538d58286f4dd546e6d3b1a3e5b3e0cde0c6
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-16 17:01:17 -0700

    Add s3 bucket

---
+++ s3.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "foundry_s3_access" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:ListAllMyBuckets",
+            "Resource": "*"
+        }, 
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:PutObject",
+                "s3:PutObjectAcl"
+            ],
+            "Resource": [
+                "arn:aws:s3:::vtt-assets",
+                "arn:aws:s3:::vtt-assets/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "vtt-assets" {
+  bucket = "my-tf-test-bucket"
+  acl    = "public-read"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "HEAD"]
+    allowed_origins = ["http://www.inharnsway.com"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
