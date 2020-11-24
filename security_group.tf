14fd01cb2b94219b469e2f95e927128f10ea6773
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-11-24 14:54:24 -0700

    Support https

--- security_group.tf
+++ security_group.tf
@@ -4,9 +4,17 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_default_vpc.default.id
 
   ingress {
-    description = "Foundry from VPC"
+    description = "HTTP to Apache"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS to Apache"
+    from_port   = 443
+    to_port     = 433
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
