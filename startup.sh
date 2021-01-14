d1f24391826e10bab7807921248135d7b208430d
Stephen Cooper <scooper4711@users.noreply.github.com>
2021-01-13 22:06:02 -0700

    last ref to server

--- startup.sh
+++ startup.sh
@@ -36,7 +36,7 @@ if ! /root/.acme.sh/acme.sh --list | grep ${name}.${domain}; then
     # Need http running to get a cert issued
     systemctl start httpd
     /root/.acme.sh/acme.sh --issue -d ${name}.${domain} -w /var/www/html --debug
-    /root/.acme.sh/acme.sh --install-cert -d ${server}.${domain} \
+    /root/.acme.sh/acme.sh --install-cert -d ${name}.${domain} \
         --cert-file /etc/pki/tls/certs/${domain}/cert.pem \
         --key-file /etc/pki/tls/certs/${domain}/key.pem  \
         --fullchain-file /etc/pki/tls/certs/${domain}/fullchain.pem
